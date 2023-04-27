f5cb0c92fecba605bd7246d10a5b3f5ed354c56a
CameronGoraya <camerongoraya@gmail.com>
2023-04-27 15:15:13 +0100

    changing long and lats of locations which are inputted incorrectly

--- location_data_formatter.py
+++ location_data_formatter.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import requests
+import json
 
 # read the CSV data from the given URL
 data = pd.read_csv("http://chargepoints.dft.gov.uk/api/retrieve/registry/format/csv", 
@@ -50,8 +52,29 @@ data = data[columns]
 
 data['access24Hours'] = data['access24Hours'].fillna(0)
 
-# Update longitude values for ChargePlace Scotland controllers
+# Update longitude values for ChargePlace Scotland controllers (values positive should be negative)
 chargeplace_scotland = data['deviceControllerName'] == 'ChargePlace Scotland'
 data.loc[chargeplace_scotland & (data['longitude'] > 2), 'longitude'] = data.loc[chargeplace_scotland & (data['longitude'] > 2), 'longitude'] * -1
+
+# Update location values for locations where long and lat wrong way around
+data.loc[data['chargeDeviceID'] == '53d474266f0eba48e4df65250fe2c56b', ['latitude', 'longitude']] = data.loc[data['chargeDeviceID'] == '53d474266f0eba48e4df65250fe2c56b', ['longitude', 'latitude']].values
+data.loc[data['chargeDeviceID'] == '98555e2a46b8f678592b7caba74504cf', ['latitude', 'longitude']] = data.loc[data['chargeDeviceID'] == '98555e2a46b8f678592b7caba74504cf', ['longitude', 'latitude']].values
+
+#Long and lat both 5, got values from postcode
+data.loc[data['chargeDeviceID'] == '906a7cf4657ab3f2f031889a0ed25057', ['latitude', 'longitude']] = [50.7851, -1.606]
+
+# Replace longitude with postcode longitude if it's over 50
+for index, row in data.iterrows():
+    if row['longitude'] > 50:
+        postcode = row['postcode']
+        postcode_request = requests.get(f"https://api.postcodes.io/postcodes/{postcode}")
+        postcode_data = json.loads(postcode_request.text)
+        if postcode_data['status'] == 200:
+            longitude = postcode_data['result']['longitude']
+            data.loc[index, 'longitude'] = longitude
+
+# Delete chargers from SureCharge with no location
+delete_SureCharge = (data['deviceControllerName'] == 'SureCharge/FM Conway') & (data['latitude'] == 0)
+data = data.drop(data[delete_SureCharge].index)
 
 data.to_csv('csv/chargers.csv', index=True)
